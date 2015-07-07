9f18d725c090cef13a40e37e76acdbf98e136fb7
LiuBing <corleone31313@gmail.com>
2015-07-07 08:31:47 +0800

    rm

--- core/IListener.go
+++
@@ -1,5 +0,0 @@
-package core
-
-type IListener interface {
-	Listen(ch <-chan string)
-}
--- drivers/IPort.go
+++
@@ -1,9 +0,0 @@
-package driver
-
-import (
-	FolderManager "github.com/Bruce313/EventHub/core/FolderManager"
-)
-
-type IPort interface {
-	Serve(fm *FolderManager)
-}
